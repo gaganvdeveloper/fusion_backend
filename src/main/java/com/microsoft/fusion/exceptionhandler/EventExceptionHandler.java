9bfc5183801686e8578ebedc2e589c33571d05aa
Nageswariv630 <nageswariv630@gmail.com>
2024-12-21 16:23:07 +0530

    Developed API for Persisting the Event And Registering the event by user

--- src/main/java/com/microsoft/fusion/exceptionhandler/EventExceptionHandler.java
+++ src/main/java/com/microsoft/fusion/exceptionhandler/EventExceptionHandler.java
@@ -1,8 +1,21 @@
 package com.microsoft.fusion.exceptionhandler;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.microsoft.fusion.exceptionclasses.NoUpComingEventFoundException;
+import com.microsoft.fusion.responsestructure.ResponseStructure;
 
 @RestControllerAdvice
 public class EventExceptionHandler {
+	@ExceptionHandler(NoUpComingEventFoundException.class)
+	public ResponseEntity<?> NoUpComingEventFoundException(NoUpComingEventFoundException e){
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).
+				body(ResponseStructure.builder().status(HttpStatus.BAD_REQUEST.value()).
+						message("No UpComing Events Found").body(e.getMessage()).build());
+	}
 
 }
